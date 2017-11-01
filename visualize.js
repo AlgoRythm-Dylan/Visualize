9fbb4e1c35814c1955a85683a9b2511c3a894b01
AlgoRythm <dperunko@comcast.net>
2017-11-01 19:19:00 -0400

    Working code

--- visualize.js
+++ visualize.js
@@ -20,8 +20,9 @@ var MusicVisualizer = {
         this.audio = audio;
         this.renderer;
         var dataArray;
-        var renderers;
-        var audioFilters;
+        this.renderer;
+        this.audioFilters = [];
+        this.paints = [];
 
         // Interact with the audio API.
         var analyzerNode;
@@ -33,15 +34,22 @@ var MusicVisualizer = {
             analyzerNode = audioContext.createAnalyser();
             audioSource.connect(analyzerNode);
             audioSource.connect(audioContext.destination);
-            new Uint8Array(analyzerNode.frequencyBinCount);
+            dataArray = new Uint8Array(analyzerNode.frequencyBinCount);
         }
         // Make the local variables public
         this.analyzerNode = analyzerNode;
         this.audioContext = audioContext;
         this.audioSource = audioSource;
+        this.dataArray = dataArray;
 
         function render(){
-            // Do stuff!
+            this.analyzerNode.getByteFrequencyData(this.dataArray);
+            for(var i = 0; i < this.audioFilters.length; i++){
+                this.audioFilters[i].apply(this.dataArray);
+            }
+            if(this.canvas != null && this.renderer != null){
+                this.renderer.render(this);
+            }
         }
         this.render = render;
     },
@@ -50,8 +58,75 @@ var MusicVisualizer = {
     //  Graphics / Renderers    //
     //                          //
     //////////////////////////////
-    SimpleBar: function(){
-        
-    }
+    SimpleBarRenderer: function(){
+        this.render = function(visualizer){
+            var canvas = visualizer.canvas;
+            var data = visualizer.dataArray;
+            var ctx = canvas.getContext("2d");
 
+            var width = canvas.width;
+            var height = canvas.height;
+            var barWidth = width / visualizer.analyzerNode.frequencyBinCount;
+            barWidth = Math.round(barWidth);
+
+            // Draw the background
+            if(visualizer.paints[MusicVisualizer.SECONDARY] == null){
+                ctx.clearRect(0, 0, width, height);
+            }
+            else{
+                ctx.fillStyle = visualizer.paints[MusicVisualizer.SECONDARY].toString();
+                ctx.fillRect(0, 0, width, height)
+            }
+
+            for(var i = 0; i < visualizer.analyzerNode.frequencyBinCount; i++){
+                var barHeight = (visualizer.dataArray[i] / 255) * height;
+                var x = barWidth * i;
+                var y = height - barHeight;
+                // Get the paint color
+                ctx.fillStyle = visualizer.paints[MusicVisualizer.PRIMARY].toString();
+                ctx.fillRect(x, y, barWidth, barHeight);
+            }
+        }
+    },
+    //////////////////////////////
+    //                          //
+    //  Paint                   //
+    //                          //
+    //////////////////////////////
+    SimplePaint: function(r, g, b, a){
+        if(r == null) r = 0;
+        if(g == null) g = 0;
+        if(b == null) b = 0;
+        if(a == null) a = 1;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a;
+        this.toString = function(){
+            return "rgba(" + this.r + ", " + this.g + ", " + this.b + ", " + this.a + ")";
+        }
+    },
+    HighPerformancePaint: function(str){
+        this.paint = str;
+        this.toString = function(){
+            return this.paint;
+        }
+    },
+    //////////////////////////////
+    //                          //
+    //  Audio filters           //
+    //                          //
+    //////////////////////////////
+    simpleDampenFilter: function(){
+        this.apply = function(data){
+            for(var i = 0; i < data.length; i++){
+                var newData = data[i];
+                newData -= 255 - newData;
+                if(newData < 0){
+                    newData = 0;
+                }
+                data[i] = newData;
+            }
+        }
+    }
 }
